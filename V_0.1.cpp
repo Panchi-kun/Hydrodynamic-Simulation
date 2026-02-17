ac3f6c41e98c8ed6652025ddbe9d7baaed2f849d
unknown <arallan@csu.fullerton.edu>
2026-02-16 22:54:22 -0800

    Added struct cell

---
+++ V_0.1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+struct Cell{
+    float FloorHeight;
+    float WaterHeight;
+    float x;
+    float y;
+    float z;
+}
+
+void source{
+
+}
+
+void sink{
+
+}
+
+int main{
+
+}
