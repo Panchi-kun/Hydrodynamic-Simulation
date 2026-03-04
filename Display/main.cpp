92de24fc43b0e4e02699d906080803334f3e7167
andrewSZ <drewsykes28@gmail.com>
2026-03-04 01:12:43 -0800

    Drew circle

--- Display/main.cpp
+++ Display/main.cpp
@@ -1,24 +1,32 @@
 #include <SFML/Graphics.hpp>
 
 int main() {
-    // SFML 3 uses Vector2u for window size
-    sf::RenderWindow window(sf::VideoMode(sf::Vector2u(800, 600)), "SFML in VSCode");
     
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+    sf::RenderWindow window(sf::VideoMode({800,600}),"My Windows");
     
-    while (window.isOpen()) {
-        // SFML 3 uses std::optional for event polling
-        while (const std::optional event = window.pollEvent()) {
-            if (event->is<sf::Event::Closed>()) {
+    window.setSize({600,600});
+    window.setTitle("Gay");
+
+
+    window.setFramerateLimit(60);
+    //window.setVerticalSyncEnabled(true);
+
+    while (window.isOpen()){
+        while (const std::optional event = window.pollEvent()){
+            if (event->is<sf::Event::Closed>())
                 window.close();
-            }
         }
-        
-        window.clear();
+
+        window.clear(sf::Color::Black);
+
+        sf::CircleShape shape(50.f);
+        shape.setFillColor(sf::Color(100,250,50));
         window.draw(shape);
+
+        
         window.display();
+        
     }
-    
+
     return 0;
 }
