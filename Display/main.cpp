71194b7648a7935d09ad56f3cdfe515d1e2f983b
andrewSZ <drewsykes28@gmail.com>
2026-03-05 11:31:59 -0800

    Created vague interaction

--- Display/main.cpp
+++ Display/main.cpp
@@ -1,11 +1,14 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
+#include <bits/stdc++.h>
+#include <cmath>
 
-unsigned int windowX = 800;
-unsigned int windowY = 600;
+unsigned int windowX = 400;
+unsigned int windowY = 400;
+int particleNum = 2;
 
 float g = 90.8f;
-float rad1 = 20.f;
+float rad = 20.f;
 
 float* ballLocational(float dt, float* info){
     info[3] += g*dt;
@@ -16,9 +19,28 @@ float* ballLocational(float dt, float* info){
 }
 
 float* wallCollision(float* info){
-    if (info[0] <= 0 || info[0]+2*rad1 >= windowX) info[2] = -info[2];
-    if (info[1] <= 0 || info[1]+2*rad1 >= windowY) info[3] = -info[3];
+    if (info[0] <= 0 || info[0]+2*rad >= windowX) info[2] = -info[2];
+    if (info[1] <= 0 || info[1]+2*rad >= windowY) info[3] = -info[3];
     return info;
+}
+
+float* ballCollision(float symInfo){
+
+}
+
+float* makeInitial(float* infoi){
+    static std::default_random_engine gen(std::random_device{}());
+    std::uniform_real_distribution<double> rPos(0.0,std::min(windowX-2*rad,windowY-2*rad));
+    std::uniform_real_distribution<double> rVel(0.0,100.0);
+
+    infoi[0] = rPos(gen); infoi[2] = rVel(gen);
+    infoi[1] = rPos(gen); infoi[3] = rVel(gen);
+    return infoi;
+}
+
+sf::CircleShape createBall(){
+    sf::CircleShape ball(rad); ball.setFillColor(sf::Color(150,50,50));
+    return ball;
 }
 
 int main() {
@@ -26,8 +48,14 @@ int main() {
     sf::RenderWindow window(sf::VideoMode({windowX,windowY}),"My Windows");
     window.setFramerateLimit(60);
 
-    float info1[4] = {200.0f,200.0f,100.0f, 100.0f};
-    float info2[4] = {100.0f,400.0f,100.0f, 100.0f};
+    float info[particleNum][4];
+    
+    for (int i = 0; i < particleNum; i++){
+        float infoi[4];
+        makeInitial(infoi);
+        info[i][0] = infoi[0]; info[i][2] = infoi[2];
+        info[i][1] = infoi[1]; info[i][3] = infoi[3];
+    }
 
     sf::Clock clock;
     sf::Time lastTime = clock.getElapsedTime();
@@ -45,24 +73,57 @@ int main() {
         window.clear(sf::Color::Black);
         
         
-        // Plot the first ball, with initial conditions
-        sf::CircleShape ball1(rad1); ball1.setFillColor(sf::Color(150,50,50));
 
-        ballLocational(dt,info1);
-        wallCollision(info1);
-        ball1.setPosition({info1[0],info1[1]});
         
-        window.draw(ball1);
-        window.display();
-
-        // Plot the second ball, with initial conditions
-        sf::CircleShape ball2(rad1); ball2.setFillColor(sf::Color(50,50,150));
-
-        ballLocational(dt,info2);
-        wallCollision(info2);
-        ball2.setPosition({info2[0],info2[1]});
         
+
+        //ballLocational(dt,info[0]);
+        //wallCollision(info1);
+            
+        //ballLocational(dt,info2);
+        //wallCollision(info2);
+
+        sf::CircleShape ball1 = createBall();
+        sf::CircleShape ball2 = createBall();
+
+        for (int i=0; i<particleNum; i++){
+            float* infoi = info[i];
+            ballLocational(dt, infoi);
+            wallCollision(infoi);
+        }
+
+        int dealt[particleNum][particleNum] = {};
+        for (int i=0; i<particleNum; i++){
+            for (int j=0; j<particleNum; j++){
+                if (i==j || dealt[i][j] == 1) continue;
+                dealt[i][j]=1;
+                dealt[j][i]=1;
+
+                float dx = abs(info[i][0] - info[j][0]);
+                float dy = abs(info[i][1] - info[j][1]);
+                
+                float Ux = dx - 2*rad;
+                float Uy = dy - 2*rad;
+                
+                if (Ux<0 && Uy<0){
+                    float nUx = 0.5f*abs(Ux);
+                    float nUy = 0.5f*abs(Uy);
+
+                    info[i][2] = info[i][2] - nUx; info[j][2] = info[j][2] + nUx;
+                    info[i][3] = info[i][3] - nUy; info[j][3] = info[j][3] + nUy;
+                    
+                    
+                }
+
+            }
+        }
+
+        ball1.setPosition({info[0][0],info[0][1]});
+        ball2.setPosition({info[1][0],info[1][1]});
+
+        window.draw(ball1);   
         window.draw(ball2);
+
         window.display();
     
     }
