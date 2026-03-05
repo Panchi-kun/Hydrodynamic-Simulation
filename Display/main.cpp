b063f0e0fdd8c305d6431eca4afb3fe48fd8c09d
andrewSZ <drewsykes28@gmail.com>
2026-03-05 02:04:15 -0800

    Created working double particle colloision

--- Display/main.cpp
+++ Display/main.cpp
@@ -4,18 +4,30 @@
 unsigned int windowX = 800;
 unsigned int windowY = 600;
 
-float g = 9.8f;
+float g = 90.8f;
 float rad1 = 20.f;
 
+float* ballLocational(float dt, float* info){
+    info[3] += g*dt;
+    info[0] += info[2]*dt;
+    info[1] += info[3]*dt;
+    
+    return info;
+}
 
+float* wallCollision(float* info){
+    if (info[0] <= 0 || info[0]+2*rad1 >= windowX) info[2] = -info[2];
+    if (info[1] <= 0 || info[1]+2*rad1 >= windowY) info[3] = -info[3];
+    return info;
+}
 
 int main() {
 
     sf::RenderWindow window(sf::VideoMode({windowX,windowY}),"My Windows");
     window.setFramerateLimit(60);
 
-    float vel[2] = {200.0f,200.0f};
-    float pos[2] = {400.0f, 400.0f};
+    float info1[4] = {200.0f,200.0f,100.0f, 100.0f};
+    float info2[4] = {100.0f,400.0f,100.0f, 100.0f};
 
     sf::Clock clock;
     sf::Time lastTime = clock.getElapsedTime();
@@ -30,26 +42,28 @@ int main() {
         float dt = (now - lastTime).asSeconds();
         lastTime = now;
 
-        vel[1] += g*dt;
-        pos[0] += vel[0]*dt;
-        pos[1] += vel[1]*dt;
-
-        if (0 >= pos[0] || pos[0]+2*rad1 >= windowX) vel[0] = -vel[0];
-        if (0 >= pos[1] || pos[1]+2*rad1 >= windowY) vel[1] = -vel[1];
-
         window.clear(sf::Color::Black);
         
         
         // Plot the first ball, with initial conditions
-        sf::CircleShape ball1(rad1);
-        ball1.setFillColor(sf::Color(150,50,50));
+        sf::CircleShape ball1(rad1); ball1.setFillColor(sf::Color(150,50,50));
 
-        ball1.setPosition({pos[0],pos[1]});
+        ballLocational(dt,info1);
+        wallCollision(info1);
+        ball1.setPosition({info1[0],info1[1]});
+        
         window.draw(ball1);
         window.display();
 
+        // Plot the second ball, with initial conditions
+        sf::CircleShape ball2(rad1); ball2.setFillColor(sf::Color(50,50,150));
 
-
+        ballLocational(dt,info2);
+        wallCollision(info2);
+        ball2.setPosition({info2[0],info2[1]});
+        
+        window.draw(ball2);
+        window.display();
     
     }
 
