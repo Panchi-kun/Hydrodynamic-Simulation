6707e5b1570e3a4141a82f1d47ea7689744067bb
andrewSZ <drewsykes28@gmail.com>
2026-03-04 23:52:24 -0800

    Functional gravitational trajectory simulator

--- Display/main.cpp
+++ Display/main.cpp
@@ -1,31 +1,46 @@
 #include <SFML/Graphics.hpp>
+#include <iostream>
+
+float* position(sf::Time time) {
+    float x_0  = 100.0f; float y_0  = 300.0f;
+    float v_x0 = 50.0f ; float v_y0 = -50.0f;
+    float g = 9.8f; 
+    
+    float sec = time.asSeconds();
+
+    float x = v_x0*sec + x_0;
+    float y = 0.5*g*sec*sec + v_y0*sec + y_0;
+
+    static float pos[2];
+    pos[0] = x;
+    pos[1] = y;
+    return pos;
+}
+
 
 int main() {
-    
+
     sf::RenderWindow window(sf::VideoMode({800,600}),"My Windows");
-    
-    window.setSize({600,600});
-    window.setTitle("Gay");
-
-
     window.setFramerateLimit(60);
-    //window.setVerticalSyncEnabled(true);
 
+    sf::Clock clock;
     while (window.isOpen()){
         while (const std::optional event = window.pollEvent()){
             if (event->is<sf::Event::Closed>())
                 window.close();
         }
-
         window.clear(sf::Color::Black);
+        
+        sf::CircleShape shape(20.f);
+        shape.setFillColor(sf::Color(150,50,50));
 
-        sf::CircleShape shape(50.f);
-        shape.setFillColor(sf::Color(100,250,50));
+        static float pos[2];
+        float* vals = position(clock.getElapsedTime()); 
+        shape.setPosition({vals[0],vals[1]});
+
         window.draw(shape);
-
-        
         window.display();
-        
+
     }
 
     return 0;
