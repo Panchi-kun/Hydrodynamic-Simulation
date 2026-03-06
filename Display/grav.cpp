b6eba56ac831312c61583e6a73791f971b3caf96
andrewSZ <drewsykes28@gmail.com>
2026-03-05 18:12:49 -0800

    Created completely functional galaxy collision

--- Display/grav.cpp
+++ Display/grav.cpp
@@ -41,7 +41,7 @@ struct Particle{
     }
 
     void reDraw(){
-        ball.setPosition({x-radius,y-radius});
+        ball.setPosition({x,y});
     }
 };
 
@@ -55,7 +55,7 @@ void trajectory(Particle& par1, Particle& par2, float dt){
     float r     = sqrt(dx*dx + dy*dy);
 
     float a1 = -G*par2.mass/(r*r); 
-    float a2 =  G*par1.mass/(r*r);
+    float a2 = G*par1.mass/(r*r);
 
     // Find updated velocities
     par1.v_x += a1*dt*cos(theta); par2.v_x += a2*dt*cos(theta);
@@ -75,50 +75,49 @@ vector<Particle> rings(Particle center, float R, int detail){
     float m1 = center.mass;
     float x0 = center.x;
     float y0 = center.y;
-
-    vector<float> drawXs(detail), drawVxs(detail);
-    vector<float> drawYs(detail), drawVys(detail);
-    for (int i=0; i<detail; i++){
-        float theta = 2*pi/detail*i;
-        float v_orbital = sqrt(G*m1/R);
-
-        drawXs[i] = x0+R*cos(theta); drawVxs[i] = -v_orbital*sin(theta);
-        drawYs[i] = y0+R*sin(theta); drawVys[i] =  v_orbital*cos(theta);
-    } 
+    float v_orbital = sqrt(G*m1/R);
 
 
     vector<Particle> parts;
     parts.push_back(center);
     for (int i=0; i<detail; i++){
-        Particle ringi(8.0f, moonMass, sf::Color(230,230,230)) ; ringi.set_pos(drawXs[i],drawYs[i]); ringi.set_vel(drawVxs[i],drawVys[i]);
+        float theta = 2*pi/detail*i;
+
+        float px = x0 + R*cos(theta);
+        float py = y0 + R*sin(theta);
+        float vx = -v_orbital*sin(theta);
+        float vy =  v_orbital*cos(theta);
+
+        Particle ringi(2.0f, moonMass, sf::Color(230,230,230));
+        ringi.set_pos(px, py);
+        ringi.set_vel(vx, vy);
         parts.push_back(ringi);
     }
-    
 
     return parts;
 }
 
 
-// Window is 800x800 px^2
-
 int main(){
 
-    Particle s1(20.0f, earthMass, sf::Color(35,137,218)); s1.set_pos(600,400); s1.set_vel(0,0) ;
-    vector<Particle> parts = rings(s1,200,2);
+    Particle s1(20.0f, earthMass, sf::Color(35,137,218)); s1.set_pos(600,200); s1.set_vel(0,0) ;
+    int nRings = 30;
+    int detail = 60;
+    float startR = 50.0f;
+    float gapR   = 8.0f;
 
+    vector<Particle> parts = rings(s1, startR, detail);
+    for (int k=1; k<nRings; k++){
+        vector<Particle> ringk = rings(s1, startR + k*gapR, detail);
+        parts.insert(parts.end(), ringk.begin()+1, ringk.end());
+    }
 
-    //Particle s2(8.0f, moonMass *pxtometer, sf::Color(230,230,230)) ; s2.set_pos(100,400); s2.set_vel(0,-100);
-    //Particle s3(8.0f, moonMass *pxtometer, sf::Color(230,230,230)) ; s3.set_pos(700,400); s3.set_vel(0,100);
-    //vector<Particle> parts = {s1, s2,s3};
-    
-    //Particle s2(8.0f, moonMass *pxtometer, sf::Color(10,50,150)) ; s2.set_pos(100,400); s2.set_vel(0,0);
-    //Particle s3(8.0f, moonMass *pxtometer, sf::Color(10,150,150)); s3.set_pos(700,400); s3.set_vel(0,0);
-    
-    //Particle s4(8.0f, moonMass *pxtometer, sf::Color(100,50,150)) ; s4.set_pos(400,100); s4.set_vel(0,0);
-    //Particle s5(8.0f, moonMass *pxtometer, sf::Color(100,150,150)); s5.set_pos(400,700); s5.set_vel(0,0);
-    
-    //vector<Particle> parts = {s1, s2, s3,s4,s5};
-
+    //Particle s2(20.0f, earthMass*.5, sf::Color(135,137,218)); s2.set_pos(1400,200); s2.set_vel(-180,80);
+    //Particle s3(20.0f, earthMass*.5, sf::Color(135,137,218)); s3.set_pos(200,200);  s3.set_vel(180,20);
+    Particle s2(20.0f, earthMass*.5, sf::Color(135,137,218)); s2.set_pos(1400,200); s2.set_vel(-80,80);
+    Particle s3(20.0f, earthMass*.5, sf::Color(135,137,218)); s3.set_pos(200,900);  s3.set_vel(80,0);
+    parts.push_back(s2);
+    parts.push_back(s3);
 
     sf::RenderWindow window(sf::VideoMode({windowX,windowY}),"My Windows");
     window.setFramerateLimit(60);
@@ -137,17 +136,30 @@ int main(){
         lastTime = now;
 
         window.clear(sf::Color::Black);
-        
+
         for (int i=0; i<parts.size(); i++){
             window.draw(parts[i].ball);
         }
 
         for (int i=0; i<parts.size(); i++){
+            float ax = 0, ay = 0;
             for (int j=0; j<parts.size(); j++){
-                if (i==j || j>i) continue;
-
-                trajectory(parts[i],parts[j],dt);
+                if (i==j) continue;
+                float dx = parts[j].x - parts[i].x;
+                float dy = parts[j].y - parts[i].y;
+                float r  = sqrt(dx*dx + dy*dy);
+                float a  = G*parts[j].mass/(r*r);
+                ax += a * dx/r;
+                ay += a * dy/r;
             }
+        parts[i].v_x += ax*dt;
+        parts[i].v_y += ay*dt;
+        }
+
+        for (int i=0; i<parts.size(); i++){
+        parts[i].x += parts[i].v_x*dt;
+        parts[i].y += parts[i].v_y*dt;
+        parts[i].ball.setPosition({parts[i].x-parts[i].radius, parts[i].y-parts[i].radius});
         }
 
         window.display();
