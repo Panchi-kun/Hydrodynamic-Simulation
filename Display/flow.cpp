ea5f232ff58c4abf253c91736da922a072219531
andrewSZ <drewsykes28@gmail.com>
2026-03-22 09:24:14 -0700

    Created working uniform flow

--- Display/flow.cpp
+++ Display/flow.cpp
@@ -10,6 +10,7 @@ static std::default_random_engine gen(std::random_device{}());
 const unsigned int windowX = 1500;
 const unsigned int windowY = 1000;
 const float pi = 3.14159f;
+const int tGap = 1;
 #pragma endregion
 
 struct Particle{
@@ -31,16 +32,20 @@ struct Particle{
     void reDraw(){ball.setPosition({x,y});}
 };
 
-vector<Particle> FlowParts(){
+vector<Particle> flowParts(){
     float mRad = 10.0f;
+    float iniPos = 50.0f;
+    float iniVx = 100.0f;
+    float iniVy = 0.0f;
+
     int num = 10;
     float linGap = windowY/num;
     
     vector<Particle> flows;
     for (int i=1; i-1<num; i++){
         Particle flowi(mRad, Color(35,137,218));
-        flowi.set_pos(100,linGap*i);
-        flowi.set_vel(70,0);
+        flowi.set_pos(iniPos,linGap*i);
+        flowi.set_vel(iniVx,iniVy);
 
         flows.push_back(flowi);
     }
@@ -53,7 +58,7 @@ int main(){
     // Track time
     Clock clock;
     Time lastTime = clock.getElapsedTime();
-    Time flowDraws = lastTime;
+    Time lastFlow = lastTime;
 
 
     // Define Particles
@@ -87,19 +92,26 @@ int main(){
 
         // Update positions
         for (int i=0; i<movers.size(); i++){
+            float x = movers[i].x;
+            float y = movers[i].y;
+            float r = movers[i].r;
+
+            if (x < 0 || windowX<x || y<0 || windowY<y){
+                movers.erase(movers.begin() + i);
+                continue;
+            }
             movers[i].x += movers[i].vx*dt; 
+            movers[i].y += movers[i].vy*dt; 
         }
+        cout << movers.size() <<endl;
 
         // Define new items
-        int tGap = 1;
-        cout <<now.asSeconds()<<endl;
-        if ((now-flowDraws).asSeconds() < tGap){
-
-            vector <Particle> flows = FlowParts();
+        if ((now-lastFlow).asSeconds() > tGap){
+            vector <Particle> flows = flowParts();
             for (auto & flow : flows) {
                 movers.push_back(flow);
-                Time flowDraws = clock.getElapsedTime();
             }
+            lastFlow = lastTime;
         }
     }
 
